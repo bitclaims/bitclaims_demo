b8ee7568f4f2e51458d265a5e08740f8a9c8d4d7
Frederick Ige <duelarm@gmail.com>
2018-05-12 19:29:31 -0400

    Update datastores.js

--- config/datastores.js
+++ config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mongo',
+    url: 'mongodb://bitclaims:bitclaims@ds061268.mlab.com:61268/bitclaims',
 
   },
 
