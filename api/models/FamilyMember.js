57a4777e69c1bb0c598bc21b4fc83bf08f1ef52f
FrederickIge <duelarm@gmail.com>
2018-05-18 22:01:36 -0400

    - added "my family section" to account settings - created FamilyMember Model - created FamilyMember Controller

---
+++ api/models/FamilyMember.js
@@ -0,0 +1,38 @@
+/**
+ * FamilyMember.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    fullName: {
+      type: 'string',
+      required: true,
+      description: 'Full representation of the user\'s name',
+      maxLength: 120,
+      example: 'Lisa Microwave van der Jenny'
+    },
+    age: {
+      type: 'number',
+      description: 'Age of the member',
+      example: 1502844074211
+    },
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
